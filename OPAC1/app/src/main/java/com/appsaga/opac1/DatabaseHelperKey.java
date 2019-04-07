8b0a574533c1db6b9c9b30d947977991b1fabbe4
aggarwalmayank <17dcs006@lnmiit.ac.in>
2019-04-07 22:56:36 +0530

    databse helper key added

--- OPAC1/app/src/main/java/com/appsaga/opac1/DatabaseHelperKey.java
+++ OPAC1/app/src/main/java/com/appsaga/opac1/DatabaseHelperKey.java
@@ -10,16 +10,14 @@ public class DatabaseHelperKey extends SQLiteOpenHelper {
     public static final String DATABASE_NAME=" key.db";
     public static final String TABLE_NAME="Key_table";
     public static final String COL_1="name";
-    public static final String COL_2=" ";
-
-    public static final String COL_3="key";
+    public static final String COL_2="parentkey";
 
     public DatabaseHelperKey(Context context) {
         super(context, DATABASE_NAME, null, 2);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME+" (name TEXT ,date TEXT,key TEXT)");
+        db.execSQL("create table " + TABLE_NAME+" (name TEXT ,parentkey TEXT)");
     }
 
     @Override
@@ -27,33 +25,34 @@ public class DatabaseHelperKey extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
     }
-    public  boolean insertData(String name,String date ,String key)
+    public  boolean insertData(String name,String key)
     {
         SQLiteDatabase db=this.getWritableDatabase();
         ContentValues contentValues= new ContentValues();
         contentValues.put(COL_1,name);
-        contentValues.put(COL_2,date);
-        contentValues.put(COL_3,key);
+        contentValues.put(COL_2,key);
         long result=db.insert(TABLE_NAME,null,contentValues);
         if(result==-1)
         {return false;}
         else
             return  true;
     }
-    public boolean update(String name,String date,String description){
+    public boolean update(String name,String date,String a){
         SQLiteDatabase db=this.getWritableDatabase();
         ContentValues contentValues= new ContentValues();
         contentValues.put(COL_1,name);
         contentValues.put(COL_2,date);
-        contentValues.put(COL_3,description);
 
-
-        db.update(TABLE_NAME,contentValues,"name = ?",new String[] {name});
+        db.update(TABLE_NAME,contentValues,"name = ?",new String[] {a});
         return  true;
     }
     public Cursor getAllData(){
         SQLiteDatabase db=this.getWritableDatabase();
         Cursor res= db.rawQuery("select * from "+TABLE_NAME,null);
         return res;
+    }
+    public  void delete(String name){
+        SQLiteDatabase db=this.getWritableDatabase();
+        db.delete(TABLE_NAME,"name =?",new String[]{name});
     }
 }
